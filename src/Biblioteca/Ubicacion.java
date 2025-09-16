c2c2484d4bbd84cb1423713bae67c8166eb036e5
neo <143235753+rh03trr@users.noreply.github.com>
2025-09-15 22:10:32 -0600

    Update Ubicacion.java

--- src/Biblioteca/Ubicacion.java
+++ src/Biblioteca/Ubicacion.java
@@ -1,21 +1,34 @@
-
 package Biblioteca;
 
-public class ubicacion {
-    String estante;
-    int fila;
-    String seccion;
+public class Ubicacion {
+    private String estante;
+    private int fila;
+    private String seccion;
     
-public ubicacion(String estante, int fila, String seccion) {
-    this.estante=estante;
-    this.fila=fila;
-    this.seccion=seccion;
+    public Ubicacion(String estante, int fila, String seccion) {
+        this.estante = estante;
+        this.fila = fila;
+        this.seccion = seccion;
     }
-public String obtenerUbicacion(){
-    return "Estante: " + estante + "\nFila: " + fila + "\nSección: " + seccion;
 
+    public String getEstante() {
+        return estante;
+    }
+    
+    public int getFila() {
+        return fila;
+    }
+    
+    public String getSeccion() {
+        return seccion;
+    }
+    
+    public String obtenerUbicacion() {
+        return "Estante: " + estante + "\nFila: " + fila + "\nSección: " + seccion;
+    }
+    
+    @Override
+    public String toString() {
+        return obtenerUbicacion();
+    }
 }
-    
-}
-
-
